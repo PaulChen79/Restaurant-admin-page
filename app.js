28b7b9a5f732aa3205a814c59ad96bf3fd0ffcad
Pin Hsu Chen <paul791111@gmail.com>
2022-05-10 15:31:09 +0800

    feat: add login page & auth middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const session = require('express-session')
 const flash = require('connect-flash')
 const methodOverride = require('method-override')
+const { getUser } = require('./helpers/auth-helpers')
 const passport = require('./config/passport')
 const routes = require('./routes/index')
 const app = express()
@@ -20,7 +21,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
 app.use((req, res, next) => {
-  res.locals.user = req.user
+  res.locals.user = getUser(req)
   res.locals.success_messages = req.success_messages
   res.locals.warning_messages = req.warning_messages
   next()
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const router = express.Router()
 const admin = require('./modules/admin')
 const user = require('./modules/user')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
-router.use('/', admin)
+router.use('/', authenticatedAdmin, admin)
 router.use('/users', user)
+
+router.get('/dashbord', authenticated, (req, res) => res.render('dashboard'))
+
+router.get('/', (req, res) => res.redirect('/dashboard'))
 
 module.exports = router
